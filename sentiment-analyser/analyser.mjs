a3c423392ea6b06a023d69508cb99b71627d8b8d
Matt Johnston <mattjohnston2000@gmail.com>
2024-07-05 15:40:00 +0100

    removes console logs from the server

--- sentiment-analyser/analyser.mjs
+++ sentiment-analyser/analyser.mjs
@@ -5,11 +5,8 @@ const Sentiment = require("sentiment");
 const sentiment = new Sentiment();
 
 export const getSentiment = async (message) => {
-  console.log(message, "<<<< message body received in getSentiment");
 
   const result = sentiment.analyze(message);
-
-  console.log(result, "<<<< getSentiment result");
 
   if (result < 0) {
     return "negativee";
